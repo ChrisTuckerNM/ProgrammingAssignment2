94bbc0f49466c79e848a1420db9a7cc627183f03
Chris <ctucker@asrt.org>
2015-06-15 22:37:28 -0600

    Made some minor tweaks to readability

--- cachematrix.R
+++ cachematrix.R
@@ -5,19 +5,30 @@
 ## makeCacheMatrix() returns a list object that stores the results and values of our matrix
 ## adapted from examples given by Roger Peng PhD, Jeff Leek Phd and Brian Caffo PhD
 ## from https://class.coursera.org/rprog-015/human_grading/view/courses/973496/assessments/3/submissions
+## my comments!
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## initialize
     m <- NULL
+    
+    ## define set, will store the matrix into the environment x
     set <- function(y) {
         x <<- y
         m <<- NULL
     }
+    
+    ## get the stored matrix
     get <- function()
         x
+    
+    ## solve and store to environment variable m
     setsolve <- function(solve)
         m <<- solve
+    
+    ## return the solved matrix
     getsolve <- function()
         m
+    #define the object fields
     list(
         set = set, get = get,
         setsolve = setsolve,
@@ -25,16 +36,22 @@ makeCacheMatrix <- function(x = matrix()) {
     )
 }
 
-
+## solve the matrix and cache the result
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the solve of 'x'
     m <- x$getsolve()
+    
+    ## return the cached result if found
     if (!is.null(m)) {
         message("getting cached data")
         return(m)
     }
+    
+    # perform the solve
     data <- x$get()
     m <- solve(data, ...)
+    
+    ## cache the result
     x$setsolve(m)
     m
 }
